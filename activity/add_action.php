b8d35a9ad3840be9a3e3db1020bf7379c02d23d2
wan <snowgirl1991718@hotmail.com>
2019-12-03 01:40:59 +0800

    testing

--- activity/add_action.php
+++ activity/add_action.php
@@ -1,34 +1,38 @@
 <?php 
-    $categoryId = $_POST["category"];
-    $name = $_POST["name"];
-    $school = $_POST["school"];
-    $price = $_POST["price"];
-    $duration = $_POST["duration"];   
-    $description = $_POST["description"];
-    $startdate = $_POST["startdate"];   
-    $enddate = $_POST["enddate"];   
-    $meal = isset($_POST['meal']) != '' ? $_POST['meal'] : '';
-    if(empty($meal))
-         $meal = "false";
-    else
-        $meal = "true";     
+    // $categoryId = $_POST["category"];
+    // $name = $_POST["name"];
+    // $school = $_POST["school"];
+    // $price = $_POST["price"];
+    // $duration = $_POST["duration"];   
+    // $description = $_POST["description"];
+    // $startdate = $_POST["startdate"];   
+    // $enddate = $_POST["enddate"];   
+    // $meal = isset($_POST['meal']) != '' ? $_POST['meal'] : '';
+    // if(empty($meal))
+    //      $meal = "false";
+    // else
+    //     $meal = "true";     
 
-    $target_dir = "../assets/images/activity/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    // $target_dir = "../assets/images/activity/";
+    // $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    // $uploadOk = 1;
+    // $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    $filename = basename($_FILES["fileToUpload"]["name"]);
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-    $filePath = "https://abacadmin.herokuapp.com/assets/images/activity/" .$filename;
+    // $filename = basename($_FILES["fileToUpload"]["name"]);
+    // if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    //     echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    // } else {
+    //     echo "Sorry, there was an error uploading your file.";
+    // }
+    // $filePath = "https://abacadmin.herokuapp.com/assets/images/activity/" .$filename;
 
-    $data = ['name'=> $name, 'categoryId'=> $categoryId, 'schoolId'=> $school, 'price'=> $price, 'duration_hrs'=> $duration, 'description'=> $description, 'start_date'=> $startdate, 'end_date'=> $enddate, 'isMeal'=> $meal, 'image'=> $filePath,];
-    require '../FirebaseCls.php';
-    $firebase = new FirebaseCls("activities");
-    $result = $firebase->addActivity($data);    
-    header("location:list.php");
+    // $data = ['name'=> $name, 'categoryId'=> $categoryId, 'schoolId'=> $school, 'price'=> $price, 'duration_hrs'=> $duration, 'description'=> $description, 'start_date'=> $startdate, 'end_date'=> $enddate, 'isMeal'=> $meal, 'image'=> $filePath,];
+    // require '../FirebaseCls.php';
+    // $firebase = new FirebaseCls("activities");
+    // $result = $firebase->addActivity($data);    
+    // header("location:list.php");
+    require '../vendor/autoload.php';
+
+    use Google\Cloud\Storage\StorageClient;
+     $storage = new StorageClient(['projectId' => 'abac-fdef3']);
 ?>
